8e1d16f1d3879351361c33b276392eb7364869cb
naveuz <az.gost@gmail.com>
2018-02-21 13:20:12 +0300

    fix static pages

--- app/controllers/static_pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class StaticPagesController < ApplicationController
-  def about
-    @link = 'http://www.thinknetica.com/'
-  end
-
-  def author
-    @profile = 'https://github.com/naveuz'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get '/about', to: 'static_pages#about'
-
-  get '/about/author', to: 'static_pages#author'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
