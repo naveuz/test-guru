36add79340ed905ab3800b6009d02a635bea58a4
naveuz <az.gost@gmail.com>
2018-02-27 21:17:29 +0300

    change method titles_of_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   def self.titles_of_category(title)
     Test.joins('INNER JOIN categories ON tests.category_id = categories.id')
-        .where('categories.title = :title', title: title)
+        .where(categories: { title: title })
         .order('tests.title DESC').pluck(:title)
   end
 end
