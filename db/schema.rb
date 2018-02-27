d9e8b46cae9397bc6f658147a83753d41b0b7cac
naveuz <az.gost@gmail.com>
2018-02-27 18:00:06 +0300

    add model PassingTest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222201510) do
+ActiveRecord::Schema.define(version: 20180227140708) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20180222201510) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passing_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
