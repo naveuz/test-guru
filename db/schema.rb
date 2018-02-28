8f88e09ce9d6ba47b1cec5bf0eebdcc35701720a
naveuz <az.gost@gmail.com>
2018-02-28 22:52:06 +0300

    add model Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227140708) do
+ActiveRecord::Schema.define(version: 20180228194046) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email", null: false
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
