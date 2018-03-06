16e788c71336bbe0efc88586f993e1c78d71ea0f
naveuz <az.gost@gmail.com>
2018-03-06 19:16:18 +0300

    fix task5

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_count
+  validate :validate_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_count
-    errors.add(:question) if question.answers.size > 4
+    errors.add(:question) if question.answers.size >= 4
   end
 end
