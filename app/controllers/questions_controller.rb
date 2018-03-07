521bd3456ccdf011f5783430978a469bf6cd0076
naveuz <az.gost@gmail.com>
2018-03-07 11:53:34 +0300

    add action show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show]
 
   def index
     render inline: "<% @test.questions.each do |question| %>
@@ -7,9 +8,17 @@ class QuestionsController < ApplicationController
                     <% end %>"
   end
 
+  def show
+    render inline: @question.body
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
