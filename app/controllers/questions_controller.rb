022ee3461a894029392377a4ce8ad2a81c8f18ef
naveuz <az.gost@gmail.com>
2018-03-07 11:40:48 +0300

    add action index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,15 @@
 class QuestionsController < ApplicationController
+  before_action :find_test
+
+  def index
+    render inline: "<% @test.questions.each do |question| %>
+                      <p><%= question.body %></p>
+                    <% end %>"
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 end
