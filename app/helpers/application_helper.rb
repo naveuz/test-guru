fa06ce243a93d8289b51e941ddf927c7d4b25dfe
naveuz <az.gost@gmail.com>
2018-03-13 00:49:28 +0300

    add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def question_header(question)
+    if question.new_record?
+      "Create New #{question.test.title} Question"
+    else
+      "Edit #{question.test.title} Question"
+    end
+  end
+
+  def current_year
+    Date.current.year
+  end
+
+  def github_url(author, repo)
+    render html: "<a href=\"https://github.com/#{author}/#{repo}\">
+                    Test Guru
+                  </a>".html_safe
+  end
 end
