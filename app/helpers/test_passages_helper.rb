3d20a9e69340fe0ace4e3b9b2677bb25a1bdd7bd
naveuz <az.gost@gmail.com>
2018-03-16 00:45:46 +0300

    add methods to test_passage

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,11 @@
 module TestPassagesHelper
+  def result_passage(result)
+    if @test_passage.passed?
+      "<span style='color: green'>#{result}%</span>
+      Тест успешно пройден".html_safe
+    else
+      "<span style='color: red'>#{result}%</span>
+      Тест не пройден".html_safe
+    end
+  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,7 +3,23 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_save :before_save_set_question
+
+  def success_percent
+    self.correct_questions * 100 / question_count
+  end
+
+  def passed?
+    success_percent > 84
+  end
+
+  def question_count
+    test.questions.size
+  end
+
+  def question_number
+    question_count - next_questions.size
+  end
 
   def completed?
     current_question.nil?
@@ -11,14 +27,17 @@ class TestPassage < ApplicationRecord
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-    self.current_question = next_question
     save!
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_save_set_question
+    self.current_question = if self.current_question.nil?
+                              test.questions.first
+                            else
+                              next_question
+                            end
   end
 
   def correct_answer?(answer_ids)
@@ -29,7 +48,11 @@ class TestPassage < ApplicationRecord
     current_question.answers.correct
   end
 
+  def next_questions
+    test.questions.order(:id).where('id > ?', current_question.id)
+  end
+
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    next_questions.first
   end
 end
