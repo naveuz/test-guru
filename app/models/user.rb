d0681db52aee1c93f839be5888644190cb0fd06e
naveuz <az.gost@gmail.com>
2018-03-24 20:07:11 +0300

    user name after sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
 
+  validates :first_name, :last_name, presence: true
+
   def test_passage(test)
     test_passages.order(created_at: :desc).find_by(test: test)
   end
