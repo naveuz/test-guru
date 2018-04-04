6ebcce58a30dda44b69311ec4b4544e5a44d3844
naveuz <az.gost@gmail.com>
2018-04-04 22:37:29 +0300

    fix error js

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -28,13 +28,15 @@ function formInlineHandler(testId) {
   var testTitle = document.querySelector('.test-title[data-test-id="'+ testId +'"]');
   var formInline = document.querySelector('.form-inline[data-test-id="'+ testId +'"]');
 
-  if (formInline.classList.contains('hide')) {
-    testTitle.classList.add('hide');
-    formInline.classList.remove('hide');
-    link.textContent = 'Отмена'
-  } else {
-    testTitle.classList.remove('hide');
-    formInline.classList.add('hide');
-    link.textContent = 'Редактировать'
+  if (formInline) {
+    if (formInline.classList.contains('hide')) {
+      testTitle.classList.add('hide');
+      formInline.classList.remove('hide');
+      link.textContent = 'Отмена';
+    } else {
+      testTitle.classList.remove('hide');
+      formInline.classList.add('hide');
+      link.textContent = 'Редактировать';
+    }
   }
 }
